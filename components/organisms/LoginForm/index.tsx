5d5a6d3e330f77d6a7e98564d0622ea6762201b0
Mayur Upadhayay <mayur123a@gmail.com>
2023-03-09 00:45:07 +0530

    chore: Creates a new login page layout

---
+++ components/organisms/LoginForm/index.tsx
@@ -0,0 +1,73 @@
+// Top level imports
+import React from 'react';
+
+// Antd
+import { Button, Checkbox, Form, Input, Row, Col } from 'antd';
+
+// Component definition
+export const LoginForm: React.FC = () => {
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    // Main renderer
+    return (
+        <Form
+            name="basic"
+            initialValues={{ remember: true }}
+            style={{ marginTop: '2rem' }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            labelCol={{ md: 8 }}
+            wrapperCol={{ md: 8, sm: 24 }}
+        >
+            <Row>
+                <Col xs={24}>
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col xs={24}>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col xs={24}>
+                    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col xs={24}>
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Col>
+            </Row>
+
+        </Form>
+    )
+}
---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+// top level imports
+import { ReactElement } from "react";
+
+// Atoms / Molecules / Organisms
+import { LoginForm } from "@/components/organisms/LoginForm";
+
+// Component definitions
+export default function Login(): ReactElement {
+    return <LoginForm />
+}
