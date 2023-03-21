e1832700a881e1b1f8425274a136974f4fc995cc
Mayur Upadhayay <mayur123a@gmail.com>
2023-03-21 23:44:38 +0530

    chore: Conditionally rendering links in header

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,6 +1,10 @@
 // top level imports
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import Link from 'next/link'
+
+// React-Redux
+import { useSelector } from 'react-redux'
+import type { RootState } from 'store/types'
 
 // AntD Components
 import { Layout, Menu } from 'antd'
@@ -10,16 +14,35 @@ const { Header: AHeader } = Layout
 import type { MenuProps } from 'antd'
 import { Logo } from '@/components/atoms/Logo'
 
-const menuLinks: MenuProps['items'] = [
-	{ key: 'login', label: 'Login' },
-	{ key: 'registration', label: 'Registration' },
-].map(({ key, label }) => ({
-	key,
-	label: <Link href={key}>{label}</Link>,
-}))
-
 // Component definition
 export function Header(): ReactElement {
+	// Constants
+	const loginRoutes = [
+		{ key: 'login', label: 'Login' },
+		{ key: 'registration', label: 'Registration' },
+	]
+
+	// state definitions
+	const [menuLinks, setMenuLinks] = useState<MenuProps['items']>([])
+
+	// Hooks
+	const user = useSelector((state: RootState) => state.user)
+	const userAuthenticated = !!user?._id
+
+	useEffect(() => {
+		const linkAtoms = userAuthenticated ? [] : loginRoutes
+		const menuLinks: MenuProps['items'] = linkAtoms.map(
+			({ key, label }) => ({
+				key,
+				label: <Link href={key}>{label}</Link>,
+			})
+		)
+
+		setMenuLinks(menuLinks)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [userAuthenticated])
+
+	// Main renderer
 	return (
 		<AHeader>
 			<Logo src="/devcon.png" />
